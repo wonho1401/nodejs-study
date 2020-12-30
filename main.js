bac4f196db7814fe81243f27f8f0121a628feb07
wonho <wonho1401@naver.com>
2020-12-30 16:38:57 +0900

    make the code clear

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 let http = require("http");
 let fs = require("fs");
 let url = require("url");
-const { template } = require("lodash");
+
+let templateHTML = (title, list, body) => {
+  return `
+<!doctype html>
+  <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+  <body>
+  <h1><a href="/">WEB</a></h1>
+  ${list}
+  ${body}
+</body>
+</html>
+`;
+};
+
+let templateList = (filelist) => {
+  let list = `<ol>`;
+
+  for (let file of filelist) {
+    list += `<li><a href="/?id=${file}">${file}</a></li>`;
+  }
+  list += `</ol>`;
+  return list;
+};
 
 let app = http.createServer(function (request, response) {
   let _url = request.url;
@@ -14,55 +40,20 @@ let app = http.createServer(function (request, response) {
       fs.readdir("./data", (err, filelist) => {
         let title = "Welcome";
         let data = "Hello. Nodejs";
-        let list = `<ol>`;
-        for (let file of filelist) {
-          list += `<li><a href="/?id=${file}">${file}</a></li>`;
-        }
-        list += `</ol>`;
 
-        let template = `
-<!doctype html>
-  <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-  <body>
-  <h1><a href="/">WEB</a></h1>
-  ${list}
-  <h2>${title}</h2>
-  <p>${data}</p>
-</body>
-</html>
-`;
+        let list = templateList(filelist);
+
+        let template = templateHTML(title, list, `<h2>${title}</h2> ${data}`);
         response.writeHead(200);
         response.end(template);
       });
     } else {
       fs.readdir("./data", (err, filelist) => {
-        let title = "Welcome";
-        let data = "Hello. Nodejs";
-        let list = `<ol>`;
-        for (let file of filelist) {
-          list += `<li><a href="/?id=${file}">${file}</a></li>`;
-        }
-        list += `</ol>`;
-        fs.readFile(`data/${title}`, "utf8", (err, data) => {
-          let template = `
-<!doctype html>
-  <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-  <body>
-  <h1><a href="/">WEB</a></h1>
-  ${list}
-  <h2>${title}</h2>
-  <p>${data}</p>
-</body>
-</html>
-`;
+        let list = templateList(filelist);
+
+        fs.readFile(`data/${queryData.id}`, "utf8", (err, data) => {
+          let title = queryData.id;
+          let template = templateHTML(title, list, `<h2>${title}</h2> ${data}`);
           response.writeHead(200);
           response.end(template);
         });
