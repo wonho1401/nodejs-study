73b2e13d8d821ab194eaab89fc2a722b40ea0303
wonho <wonho1401@naver.com>
2021-01-29 10:10:06 +0900

    session example

--- express-session.js
+++ express-session.js
@@ -8,12 +8,19 @@ app.use(
   session({
     secret: "Keyboard Cat", //필수적으로 들어가야하는 값. 나중에는 꼭 변수처리하고 공유되지 않게 해야함.
     resave: false,
-    saveUninitialized: true,    //true로 놔야 세션이 있을때만 실행이 된다. false로 두게되면 서버에 큰 부담을 줄 수 있음.
+    saveUninitialized: true, //true로 놔야 세션이 있을때만 실행이 된다. false로 두게되면 서버에 큰 부담을 줄 수 있음.
   })
 );
 
+//
 app.get("/", (req, res, next) => {
-  res.send("Hello Session");
+  if (req.session.num === undefined) {
+    req.session.num = 1;
+  } else {
+    req.session.num += 1;
+  }
+
+  res.send(` View = ${req.session.num}`);
 });
 
 app.listen(3000, () => {
