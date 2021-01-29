398bd651877dbe7c717547d85af4c2b4fbda59ce
wonho <wonho1401@naver.com>
2021-01-29 10:28:21 +0900

    session file store

--- express-session.js
+++ express-session.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const parseurl = require("parseurl");
 const session = require("express-session");
-
+const FileStore = require("session-file-store")(session);
 const app = express();
 
 app.use(
@@ -9,6 +9,7 @@ app.use(
     secret: "Keyboard Cat", //필수적으로 들어가야하는 값. 나중에는 꼭 변수처리하고 공유되지 않게 해야함.
     resave: false,
     saveUninitialized: true, //true로 놔야 세션이 있을때만 실행이 된다. false로 두게되면 서버에 큰 부담을 줄 수 있음.
+    store: new FileStore(),
   })
 );
 
